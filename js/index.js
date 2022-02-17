2de0d2d297cca6126dda16252b27a4e04c8faa50
abdullaalfahad <abdullaalfahad.cse@gmail.com>
2022-02-17 17:44:28 +0600

    all function added

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ function getInput(inputId) {
     const inputValue = document.getElementById(inputId);
     const inputText = inputValue.value;
     const inputNumber = parseFloat(inputText);
-    inputValue.value = '';
+    // inputValue.value = '';
     return inputNumber;
 }
 
@@ -15,41 +15,64 @@ function addValue(firstNum, secondNum, thirdNum) {
 }
 
 // subtract value
-function subtractValue(firstValue, secondValue) {
+function subtractValue(firstValue, secondValue, balanceId) {
     const substract = firstValue - secondValue;
-    document.getElementById('balance').innerText = substract;
+    document.getElementById(balanceId).innerText = substract;
+    return substract;
+}
+
+// save money in percent
+function saveMoney(income, saveValue) {
+    const save = saveValue / 100;
+    const saveAmount = income * save;
+    document.getElementById('saving-amount').innerText = saveAmount;
+    return saveAmount;
+}
+
+function totalCost() {
+    const foodCost = getInput('food-cost');
+    const rentCost = getInput('rent-cost');
+    const clothesCost = getInput('clothes-cost');
+    const totalExpenses = addValue(foodCost, rentCost, clothesCost);
+    return totalExpenses;
 }
 
 // handle calculate
 document.getElementById('calculate-button').addEventListener('click', function () {
     const incomeBalance = getInput('income-balance');
-    const foodCost = getInput('food-cost');
-    const rentCost = getInput('rent-cost');
-    const clothesCost = getInput('clothes-cost');
-    let errorMsgs = document.getElementById('error');
-    if (typeof incomeBalance.value != 'number' || incomeBalance < 0) {
-        errorMsgs.style.display = 'block';
-        errorMsgs.innerText = 'Provide a positive number in Income';
-    }
-    else if (typeof foodCost.value != 'number' || foodCost < 0) {
-        errorMsgs.style.display = 'block';
-        errorMsgs.innerText = 'Provide a positive number in Food';
-    }
-    else if (typeof rentCost.value != 'number' || rentCost < 0) {
-        errorMsgs.style.display = 'block';
-        errorMsgs.innerText = 'Provide a positive number in rent';
-    }
-    else if (typeof clothesCost.value != 'number' || clothesCost < 0) {
-        errorMsgs.style.display = 'block';
-        errorMsgs.innerText = 'Provide a positive number in clothes';
-    }
+
+    // let errorMsgs = document.getElementById('error');
+    // if (typeof incomeBalance.value != 'number' || incomeBalance < 0) {
+    //     errorMsgs.style.display = 'block';
+    //     errorMsgs.innerText = 'Provide a positive number in Income';
+    // }
+    // else if (typeof foodCost.value != 'number' || foodCost < 0) {
+    //     errorMsgs.style.display = 'block';
+    //     errorMsgs.innerText = 'Provide a positive number in Food';
+    // }
+    // else if (typeof rentCost.value != 'number' || rentCost < 0) {
+    //     errorMsgs.style.display = 'block';
+    //     errorMsgs.innerText = 'Provide a positive number in rent';
+    // }
+    // else if (typeof clothesCost.value != 'number' || clothesCost < 0) {
+    //     errorMsgs.style.display = 'block';
+    //     errorMsgs.innerText = 'Provide a positive number in clothes';
+    // }
     // if (typeof foodCost != 'number' || typeof rentCost != 'number' || typeof clothesCost != 'number') {
     //     errorMsgs.style.display = 'block';
     //     errorMsgs.innerText = 'Provide a positive number in Income';
     // }
+    const totalExpensesResult = totalCost();
+    subtractValue(incomeBalance, totalExpensesResult, 'balance');
+})
 
-    else {
-        const totalExpenses = addValue(foodCost, rentCost, clothesCost);
-        subtractValue(incomeBalance, totalExpenses);
-    }
+document.getElementById('save-button').addEventListener('click', function () {
+    const incomeBalance = getInput('income-balance');
+    const savePercent = getInput('save-input');
+    const savingAmount = saveMoney(incomeBalance, savePercent);
+
+    const totalExpensesResult = totalCost();
+    const balance = subtractValue(incomeBalance, totalExpensesResult, 'balance');
+    const remainingBalance = subtractValue(balance, savingAmount, 'remaining-balance');
+
 })

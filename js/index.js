0069af26c07d9522564832807724aa8c3766978a
abdullaalfahad <abdullaalfahad.cse@gmail.com>
2022-02-17 11:53:46 +0600

    js total expense added

---
+++ js/index.js
@@ -0,0 +1,55 @@
+// get input function
+function getInput(inputId) {
+    const inputValue = document.getElementById(inputId);
+    const inputText = inputValue.value;
+    const inputNumber = parseFloat(inputText);
+    inputValue.value = '';
+    return inputNumber;
+}
+
+// sum of all value
+function addValue(firstNum, secondNum, thirdNum) {
+    const sum = firstNum + secondNum + thirdNum;
+    document.getElementById('total-expenses').innerText = sum;
+    return sum;
+}
+
+// subtract value
+function subtractValue(firstValue, secondValue) {
+    const substract = firstValue - secondValue;
+    document.getElementById('balance').innerText = substract;
+}
+
+// handle calculate
+document.getElementById('calculate-button').addEventListener('click', function () {
+    const incomeBalance = getInput('income-balance');
+    const foodCost = getInput('food-cost');
+    const rentCost = getInput('rent-cost');
+    const clothesCost = getInput('clothes-cost');
+    let errorMsgs = document.getElementById('error');
+    if (typeof incomeBalance.value != 'number' || incomeBalance < 0) {
+        errorMsgs.style.display = 'block';
+        errorMsgs.innerText = 'Provide a positive number in Income';
+    }
+    else if (typeof foodCost.value != 'number' || foodCost < 0) {
+        errorMsgs.style.display = 'block';
+        errorMsgs.innerText = 'Provide a positive number in Food';
+    }
+    else if (typeof rentCost.value != 'number' || rentCost < 0) {
+        errorMsgs.style.display = 'block';
+        errorMsgs.innerText = 'Provide a positive number in rent';
+    }
+    else if (typeof clothesCost.value != 'number' || clothesCost < 0) {
+        errorMsgs.style.display = 'block';
+        errorMsgs.innerText = 'Provide a positive number in clothes';
+    }
+    // if (typeof foodCost != 'number' || typeof rentCost != 'number' || typeof clothesCost != 'number') {
+    //     errorMsgs.style.display = 'block';
+    //     errorMsgs.innerText = 'Provide a positive number in Income';
+    // }
+
+    else {
+        const totalExpenses = addValue(foodCost, rentCost, clothesCost);
+        subtractValue(incomeBalance, totalExpenses);
+    }
+})
